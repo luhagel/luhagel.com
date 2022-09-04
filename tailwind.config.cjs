8e22ebb8107e2acf94b2f8d5dbdfb9dc377e2fee
Luca Hagel <luca@luhagel.com>
2022-09-04 20:31:58 +0200

    Initial Commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily: {
+			sans: ['"Darker Grotesque"']
+		},
+		extend: {
+			colors: {
+				twitter: '#1DA1F2',
+				linkedin: '#0072b1',
+				twitch: '#6441a5',
+				pilotblue: '#4F94E4',
+				base: '#EFF1F5',
+				neutral: '#4C4F69',
+				primary: '#179299'
+			}
+		},
+	},
+	plugins: [],
+}
